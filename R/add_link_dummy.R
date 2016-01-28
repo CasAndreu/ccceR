8b2e83733150570db32c7388613f1262aeb090b4
CasAndreu <acasas2@uw.edu>
2016-01-28 11:25:13 -0800

    initial commit

---
+++ R/add_link_dummy.R
@@ -0,0 +1,20 @@
+#' A function to add a Link dummy variable.
+#'
+#' This function allows you to add a dummy variable to 
+#'    your dataset to indicate with messages contain links to URLs.
+#'    If the message contains a link, link_dummy == 1. 
+#' @param dataset The name of your dataset.
+#' @param var_text A string indicating the name of the 
+#'    variable in your dataset that conatins text messages.
+#' @keywords links
+#' @export
+#' @examples
+#' dataset_with_link_dummy <- add_link_dummy(orig_dataset, "Contents")
+#' 
+add_link_dummy <- function(dataset, var_text) {
+  dataset$link_dummy <- 0
+  texts <- dataset[ , names(dataset) == var_text]
+  texts_link <- grepl(pattern = "http", x = texts)
+  dataset$link_dummy[texts_link] <- 1
+  return(dataset)
+}
---
+++ R/add_rt_dummy.R
@@ -0,0 +1,21 @@
+#' A function to add a RT dummy variable.
+#'
+#' This function allows you to add a dummy variable to 
+#'    your dataset to indicate with messages are RTs.
+#'    If the message is a retweet, RT == 1. 
+#' @param dataset The name of your dataset.
+#' @param var_text A string indicating the name of the 
+#'    variable in your dataset that conatins text messages.
+#' @keywords retweet
+#' @export
+#' @examples
+#' dataset_with_rt_dummy <- add_rt_dummy(orig_dataset, "Contents")
+
+add_rt_dummy <- function(dataset, var_text) {
+  dataset$RT <- 0
+  texts <- dataset[ , names(dataset) == var_text]
+  texts_start <- substring(text = texts, first = 1, last = 2)
+  texts_rt <- texts_start == 'RT'
+  dataset$RT[texts_rt] <- 1
+  return(dataset)
+}
